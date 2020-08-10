100a65e42688820cafeeda3ccc5a534d999adf11
Sathyaa <st630499@dal.ca>
2020-08-10 01:37:28 -0300

    rabin karp substring equality checker

--- Coursera_DataStructures/week3_hash_tables/4_substring_equality/substring_equality.java
+++ Coursera_DataStructures/week3_hash_tables/4_substring_equality/substring_equality.java
@@ -5,15 +5,14 @@ public class substring_equality {
 	public class Solver {
 		private String s;
 		private SubStringFinder finder;
+		
 		public Solver(String s) {
 			this.s = s;
 			this.finder = new SubStringFinder(s);
 		}
+		
 		public boolean ask(int a, int b, int l) {
 		 	return finder.isSubStringEqual(a,b,l);
-			
-			
-			//return s.substring(a, a + l).equals(s.substring(b, b + l));
 		}
 	}
 
@@ -41,85 +40,111 @@ public class substring_equality {
 		private int textLen;
 		private long prime1;
 		private long prime2;
-		private long[] hashes;
+		private long[] hashes_p1;
+		private long[] powers_p1;
+		private long[] hashes_p2;
+		private long[] powers_p2;
 		private int randomNumber;
 		
 		public SubStringFinder(String s) {
 			this.text    = s;
 			this.prime1  = 1000000007;
 			this.prime2  = 1000000009;
-			this.randomNumber = 256;
+			this.randomNumber = 13;
 			
 			this.textLen = s.length();
-			this.hashes  = new long[textLen+1];
-			hashes = preComputeHashes(this.text, this.prime1);
+			this.hashes_p1  = new long[textLen+1];
+			this.powers_p1  = new long[textLen+1];
+			this.hashes_p2  = new long[textLen+1];
+			this.powers_p2  = new long[textLen+1];
+			calculate_powers(this.randomNumber,this.textLen,  this.prime1, this.powers_p1);
+			calculate_powers(this.randomNumber,this.textLen,  this.prime2, this.powers_p2);
+			
+			preComputeHashes(this.text);
 		}
+		
+		
 		
 		public boolean isSubStringEqual(int a, int b, int l) {
 			if ( a+l > textLen || b+l > textLen)
 				return false;
-			long xl = (NoPowMod(randomNumber, l, prime1));
-			long hash1 = (hashes[a+l-1] - (xl*hashes[a])%prime1) % prime1;
-			if(hash1 <0)
-				hash1 = (((hash1 % prime1) + prime1) % prime1) ;
-			long hash2 = (hashes[b+l-1] - (xl*hashes[b])%prime1) % prime1;
-			if(hash2 <0)
-				hash2 =  (((hash2 % prime1) + prime1) % prime1) ;
-			if(hash1 == hash2)
-				return true;
+			
+			long hash1 = getSubStringHash(hashes_p1, a,l, prime1, powers_p1); 
+			
+			long hash2 = getSubStringHash(hashes_p1, b,l, prime1, powers_p1);
+			
+			// if both hash is same, then do find hash with second hash function (different prime) and check for collision.
+			if(hash1 == hash2) {
+				
+				hash1 = getSubStringHash(hashes_p2, a,l, prime2, powers_p2); 
+				
+				hash2 = getSubStringHash(hashes_p2, b,l, prime2, powers_p2); 
+				
+				if(hash1 == hash2)
+					return true;
+				
+			}
 			
 			return false;
 			
 		}
 		
-		private long[] preComputeHashes(String text, long prime) {
-			long hash = 0;
-			hashes[0] = 0;
-			long a;
+		private long getSubStringHash(long[] hashes, int from, int len, long prime, long[] powers) {
+			long hash = (hashes[from+len] - (hashes[from])%prime) % prime;
+			if(hash <0)    // if hash is negative, convert to positive
+				hash = (((hash % prime) + prime) % prime) ;
+			
+			// convert all hashes to the same degree
+			hash = (hash * powers[textLen-from]) % prime;
+			return hash;
+			
+		}
+		
+		
+		private void  preComputeHashes(String text) {
+			long hash_p1 = 0;
+			long hash_p2 = 0;
+		
+			hashes_p1[0] = 0;
+			hashes_p2[0] = 0;
+			
+			long a, b;
 			
 			for(int i=0; i< textLen; i++) {
-				a = (long) (text.charAt(i) * (NoPowMod(randomNumber, textLen-i-1, prime)));
+				a = (long) ((text.charAt(i)-'a'+1) * (powers_p1[i]));
 				if(a >= 0)
-					hash = ((hash % prime) + (a % prime)) % prime ;
+					hash_p1 = ((hash_p1 % prime1) + (a % prime1)) % prime1 ;
 				else
-					hash = ((hash % prime) + ((a % prime) + prime) % prime) % prime ;
+					hash_p1 = ((hash_p1 % prime1) + ((a % prime1) + prime1) % prime1) % prime1 ;
 				
-				hashes[i+1] = hash;
+				hashes_p1[i+1] = hash_p1;
+				
+				
+				
+				b = (long) ((text.charAt(i)-'a'+1) * (powers_p2[i]));
+				if(b >= 0)
+					hash_p2 = ((hash_p2 % prime2) + (b % prime2)) % prime2 ;
+				else
+					hash_p2 = ((hash_p2 % prime2) + ((b % prime2) + prime2) % prime2) % prime2 ;
+				
+				hashes_p2[i+1] = hash_p2;
+				
 			}
-			
-			return hashes;
 			
 		}
 		
-		private long hashFunc(String s, long prime, int randomNum) {
-			long hash = 0;
-			long a = 0;
-			for (int i = 0; i < s.length() ; i++) {
-				a = (long) (s.charAt(i) * (NoPowMod(randomNum, i, prime)));
-				if(a >= 0)
-					hash = ((hash % prime) + (a % prime)) % prime ;
-				else
-					hash = ((hash % prime) + ((a % prime) + prime) % prime) % prime ; // to avoid mod of negative numbers (a%p) -> ((a %p
-																		// )+p )%p
-				
-			}
-			return hash ;
-		}
-		
-		
-		private long NoPowMod( long x, long pow, long mod )
+		private void calculate_powers( long x, long pow, long mod, long[] powers )
 		{
-			long a = x % mod;
-			long t = 1;
-			while( pow > 0 ){
-				if( (pow & 1)==1 )
-					t = (t * a) % mod;
-				
-				pow >>= 1;
-				a = (a * a) % mod;
+			long val=1;
+			powers[0] = 1;
+			for(int j =1 ; j <= pow; j++) {
+				val = (long) (powers[j-1] * x) % mod;
+				if(val <0);
+					val = (mod + val) % mod;
+				powers[j] = val;
 			}
-			return(t);
 		}
+		
 		
 	}
 
